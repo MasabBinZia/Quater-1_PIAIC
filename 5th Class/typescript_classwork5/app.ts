b72d004eddfc4322b031b2a55347c8a9ed9b109f
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-07 23:00:24 +0500

    class5work

---
+++ 5th Class/typescript_classwork5/app.ts
@@ -0,0 +1,48 @@
+// function revised
+
+function sum(num1: number, num2: number): number {
+    const r = num1 + num2;
+    return r;
+}
+const result = sum(111, 8);
+// console.log(result);
+
+if (result > 100) {
+    // console.log("this num is large");
+}
+
+// Array -splice and slicing Arrays
+let fruits: (string | number)[] = ["apple", "mango", "banna", "orange"];
+fruits.push(1)
+// console.log(fruits);
+
+let colours: string[] = ["red", "blue", "green", "yellow"];
+colours.splice(2, 0, "purple", "black");//1st argument is where to start ,2nd argument what to delete ,3rd argument what to add in array.
+// console.log(colours);
+
+let cars: string[] = ["Honda", "BMW", "Toyota", "Mercedes"];
+const car = cars.slice(1, 3)
+// console.log(car);
+
+function add(num1: number, num2 = 4) {
+    return num1 + num2;
+}
+add(2);
+
+// Loops
+//for loops
+for (let i = 0; i < 3; i++) {
+    // console.log("Hello Word");
+}
+
+
+let cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Multan"];
+for (let i = 0; i < cleanestCities.length; i++) {
+    // console.log(cleanestCities[i]);
+    if ("Islamabad" === cleanestCities[i]) {
+        console.log("yes it in the list")
+        break;
+    } else {
+        console.log("city not found");
+    }
+}
