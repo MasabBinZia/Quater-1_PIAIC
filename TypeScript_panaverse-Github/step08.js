90312e2959a59bcd11178f8d4742a08722a50bc3
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-13 20:24:59 +0500

    TS-panaverse-github07

---
+++ TypeScript_panaverse-Github/step08.js
@@ -0,0 +1,7 @@
+var array1 = [5, 6, 7]; //correct syntax
+console.log(array1[1]); //correct syntax
+var array2 = [1, 2, 3]; //alternative correct syntax
+var array3 = []; //correct syntax to define an empty array
+// let array4: number[] = new number[2];//error
+var array5 = [];
+array5.push(1234); //dynamically adding
