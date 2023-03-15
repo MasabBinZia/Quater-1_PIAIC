a8d43870bbf70458913a0c7610a58b769e6d3f70
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-03-15 17:43:44 +0500

    TS-panaverse-github07

---
+++ TypeScript_panaverse-Github/step09a.js
@@ -0,0 +1,22 @@
+//Note: All parameters are required
+//Named function
+function add(x, y) {
+    return x + y;
+}
+//Anonymous function
+var myAdd1 = function (x, y) {
+    return x + y;
+};
+//Anonymous function with explict type
+var myAdd2 = function (x, y) {
+    return x + y;
+};
+//type names dont matter
+var myAdd3 = function (x, y) {
+    return x + y;
+};
+//Lambda functions															
+var myAdd4 = function (a, b) { return a + b; };
+function greeter(fn) {
+    //....
+}
