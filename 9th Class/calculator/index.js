7aed276b6ff079f464f1e88e97583d796d886a37
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-04-04 19:07:37 +0500

    class09-cal

---
+++ 9th Class/calculator/index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+import { add } from "./add.js";
+const Answers = await inquirer.prompt([
+    {
+        name: "num1",
+        message: "Enter your First Number",
+        type: "number"
+    },
+    {
+        name: "num2",
+        message: "Enter your Second Number",
+        type: "number"
+    },
+]);
+const result = add(Answers.num1, Answers.num2);
+console.log(result);
+// npm login 
