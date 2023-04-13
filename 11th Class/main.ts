f5ee32ad15082987380b1b416dcb6443139c0310
unknown <masabmbz5@gmail.com>
2023-04-13 23:33:53 +0500

    class-11

---
+++ 11th Class/main.ts
@@ -0,0 +1,16 @@
+let obj1 :{name:string,age:number};
+let obj : {name:"masab",age:21,height:""}
+
+let array1 : number[] = [5, 6, 7];
+console.log(array1[1]);
+let array2:Array<number> = [1, 2, 3];
+let array3 : number[] = [];
+let array5: number[] = [];
+array5.push(1234);
+
+const mzb = (a:number,b:number):number => a+b
+
+const mzb1 = (a:number,b:number):number => {
+    console.log("sda");
+    return a+b;
+}
