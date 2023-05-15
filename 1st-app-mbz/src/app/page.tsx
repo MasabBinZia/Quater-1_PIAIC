77444842eb6c3ffa082325f9b371824e8c203ec9
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-05-15 23:54:29 +0500

    nextjs

---
+++ 1st-app-mbz/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className="main">
+      <h1>Hello World!</h1>
+      <h2>Made by Masab Bin Zia</h2>
+    </div>
+  )
+}
